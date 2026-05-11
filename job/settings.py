5f1ed1ee04b61747ee7ef5982c51dbcb11a4cc2b
jajiteks <jajiteks@gmail.com>
2026-05-11 22:09:04 +0530

    fixed database config

--- job/settings.py
+++ job/settings.py
@@ -95,9 +95,9 @@ WSGI_APPLICATION = 'job.wsgi.application'
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=os.getenv("DATABASE_URL"),
+        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}",
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 
